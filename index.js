1d7907769b30c2b62e4f48ef69d8f5ee4f8ee24f
leerivera <leerivera80@gmail.com>
2022-07-05 07:10:15 -0400

    get another

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.send("The Crib")
 })
 
+app.get('/dogs', (req, res) => {
+    res.send('grrr bark bark!')
+}) 
+
 app.listen(3000, () =>{
     console.log("we fast boi!!")
 })

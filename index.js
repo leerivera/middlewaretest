0077ff3bc7bf71132ae24f9f581e761af7904c53
leerivera <leerivera80@gmail.com>
2022-07-08 12:45:39 -0400

    404

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(morgan('common'))
 //the next line literally moves on the next request
 app.use((req, res, next) =>{
     console.log("I got next")
-    next();
+    return  next(); // makes sure nothing happens after next
 })
 
 app.get('/', (req, res) => {
@@ -19,6 +19,12 @@ app.get('/', (req, res) => {
 app.get('/dogs', (req, res) => {
     res.send('grrr bark bark!')
 }) 
+
+// keep the 404 at the end to catch errors in routing
+
+app.use((req, res) => {
+    res.status(404).send('What you looking for bruh')
+})
 
 app.listen(3000, () =>{
     console.log("we fast boi!!")
